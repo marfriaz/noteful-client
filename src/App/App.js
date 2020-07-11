32ade426814fc5a3528e118dd3c4eba12809c138
Marco Friaz <marfriaz@gmail.com>
2020-07-11 13:53:29 -0700

    Context added

--- src/App/App.js
+++ src/App/App.js
@@ -7,15 +7,7 @@ import NoteListMain from "../NoteListMain/NoteListMain";
 import NotePageMain from "../NotePageMain/NotePageMain";
 import dummyStore from "../dummy-store";
 import "./App.css";
-
-const findNote = (notes = [], noteId) =>
-  notes.find((note) => note.id === noteId);
-
-const findFolder = (folders = [], folderId) =>
-  folders.find((folder) => folder.id === folderId);
-
-export const getNotesForFolder = (notes = [], folderId) =>
-  !folderId ? notes : notes.filter((note) => note.folderId === folderId);
+import config from "../config";
 
 export default class App extends Component {
   state = {
@@ -24,78 +16,74 @@ export default class App extends Component {
   };
 
   componentDidMount() {
-    // fake date loading from API call
-    setTimeout(() => this.setState(dummyStore), 600);
+    Promise.all([
+      fetch(`${config.API_ENDPOINT}/notes`),
+      fetch(`${config.API_ENDPOINT}/folders`),
+    ])
+      .then(([notesRes, foldersRes]) => {
+        if (!notesRes.ok) return notesRes.json().then((e) => Promise.reject(e));
+        if (!foldersRes.ok)
+          return foldersRes.json().then((e) => Promise.reject(e));
+
+        return Promise.all([notesRes.json(), foldersRes.json()]);
+      })
+      .then(([notes, folders]) => {
+        this.setState({ notes, folders });
+      })
+      .catch((error) => {
+        console.error({ error });
+      });
   }
 
+  handleDeleteNote = (noteId) => {
+    this.setState({
+      notes: this.state.notes.filter((note) => note.id !== noteId),
+    });
+  };
+
   renderNavRoutes() {
-    const { notes, folders } = this.state;
     return (
       <>
         {["/", "/folder/:folderId"].map((path) => (
-          <Route
-            exact
-            path={path}
-            render={(routeProps) => (
-              <NoteListNav folders={folders} notes={notes} {...routeProps} />
-            )}
-          />
+          <Route exact path={path} component={NoteListNav} />
         ))}
-        <Route
-          path="/note/:noteId"
-          render={(routeProps) => {
-            const { noteId } = routeProps.match.params;
-            const note = findNote(notes, noteId) || {};
-            const folder = findFolder(folders, note.folderId);
-
-            return <NotePageNav {...routeProps} folder={folder} />;
-          }}
-        />
+        <Route path="/note/:noteId" component={NotePageNav} />
         <Route path="/add-folder" component={NotePageNav} />
-
         <Route path="/add-note" component={NotePageNav} />
       </>
     );
   }
 
   renderMainRoutes() {
-    const { notes } = this.state;
     return (
       <>
         {["/", "/folder/:folderId"].map((path) => (
-          <Route
-            exact
-            path={path}
-            render={(routeProps) => {
-              const { folderId } = routeProps.match.params;
-              const notesForFolder = getNotesForFolder(notes, folderId);
-              return <NoteListMain {...routeProps} notes={notesForFolder} />;
-            }}
-          />
+          <Route exact path={path} component={NoteListMain} />
         ))}
-        <Route
-          path="/note/:noteId"
-          render={(routeProps) => {
-            const { noteId } = routeProps.match.params;
-            const note = findNote(notes, noteId);
-            return <NotePageMain {...routeProps} note={note} />;
-          }}
-        />
+        <Route path="/note/:noteId" component={NotePageMain} />
       </>
     );
   }
 
   render() {
+    const value = {
+      notes: this.state.notes,
+      folders: this.state.folders,
+      deleteNote: this.handleDeleteNote,
+    };
     return (
-      <div className="App">
-        <nav className="App__nav">{this.renderNavRoutes()}</nav>
-        <header className="App__header">
-          <h1>
-            <Link to="/">Noteful</Link> <FontAwesomeIcon icon="check-double" />
-          </h1>
-        </header>
-        <main className="App__main">{this.renderMainRoutes()}</main>
-      </div>
+      <ApiContext.Provider value={value}>
+        <div className="App">
+          <nav className="App__nav">{this.renderNavRoutes()}</nav>
+          <header className="App__header">
+            <h1>
+              <Link to="/">Noteful</Link>{" "}
+              <FontAwesomeIcon icon="check-double" />
+            </h1>
+          </header>
+          <main className="App__main">{this.renderMainRoutes()}</main>
+        </div>
+      </ApiContext.Provider>
     );
   }
 }
--- src/NoteListNav/NoteListNav.js
+++ src/NoteListNav/NoteListNav.js
@@ -2,41 +2,45 @@ import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import CircleButton from "../CircleButton/CircleButton";
+import { countNotesForFolder } from "../notes-helpers";
+import ApiContext from "../ApiContext";
 import "./NoteListNav.css";
 
-const countNotesForFolder = (notes = [], folderId) =>
-  notes.filter((note) => note.folderId === folderId).length;
+export default class NoteListNav extends React.Component {
+  static contextType = ApiContext;
 
-export default function NoteListNav(props) {
-  return (
-    <div className="NoteListNav">
-      <ul className="NoteListNav_list">
-        {props.folders.map((folder) => (
-          <li key={folder.id}>
-            <NavLink
-              className="NoteListNav__folder-link"
-              to={`/folder/${folder.id}`}
-            >
-              <span className="NoteListNav__num-notes">
-                {countNotesForFolder(props.notes, folder.id)}
-              </span>
-              {folder.name}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-      <div className="NoteListNav__button-wrapper">
-        <CircleButton
-          tag={Link}
-          to="/add-folder"
-          type="button"
-          className="NoteListNav__add-folder-button"
-        >
-          <FontAwesomeIcon icon="plus" />
-          <br />
-          Folder
-        </CircleButton>
+  render() {
+    const { folders = [], notes = [] } = this.context;
+    return (
+      <div className="NoteListNav">
+        <ul className="NoteListNav__list">
+          {folders.map((folder) => (
+            <li key={folder.id}>
+              <NavLink
+                className="NoteListNav__folder-link"
+                to={`/folder/${folder.id}`}
+              >
+                <span className="NoteListNav__num-notes">
+                  {countNotesForFolder(notes, folder.id)}
+                </span>
+                {folder.name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+        <div className="NoteListNav__button-wrapper">
+          <CircleButton
+            tag={Link}
+            to="/add-folder"
+            type="button"
+            className="NoteListNav__add-folder-button"
+          >
+            <FontAwesomeIcon icon="plus" />
+            <br />
+            Folder
+          </CircleButton>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
